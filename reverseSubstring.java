70e1de4de55f7e49b0d4808567537a368909b2de
Abhishek Mani <aabhishekmani@gmail.com>
2022-06-13 17:52:09 +0530

    Chnages

--- reverseSubstring.java
+++ reverseSubstring.java
@@ -5,20 +5,22 @@
 public class reverseSubstring {
     public static void main(String[] args) {
         System.out.println("Abhishek Mani");
-        String str = "sweet";   
-        reverseString(str);
+        String str = "sweet";  
+        int n =3; 
+        reverseString(str,n);
         
     }
-    public static void reverseString(String s){
+    public static String reverseString(String s,int n){
         
-        
-        for (int i = 0; i < 3; i++) {
+        int size = s.length()-(n-1);
+        for (int i = 0; i < size; i++) {
             String sub1 = s.substring(0,i);
-            String sub2 = reverse(s.substring(i, i+3));
-            String sub3 = s.substring(i+3);
+            String sub2 = reverse(s.substring(i, i+n));
+            String sub3 = s.substring(i+n);
             s = sub1+sub2+sub3;
             System.out.println(s);
         }
+        return s;
     }
     public static String reverse(String s){
         String rev = "";

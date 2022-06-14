0e13d11a4587669dae27ffbc830bc781e7ccaa39
Abhishek Mani <aabhishekmani@gmail.com>
2022-06-14 23:04:48 +0530

    changes

--- Concepts/Strings/stringFunction.java
+++ Concepts/Strings/stringFunction.java
@@ -3,7 +3,7 @@ public class stringFunction {
     public static void main(String[] args) {
 
         demoOnString demo = new demoOnString();
-
+        // Demonstraton on substring means dividing string into multiple string
         String forsSubstring = "Abhishek Mani";
         demo.substring(forsSubstring);
 
@@ -15,8 +15,10 @@ class demoOnString{
 
     public void substring(String str){
         System.out.println(str);
+        // String contain 0th index to 8th which is "Abhishek".
         String sub = str.substring(0,8);
         System.out.println(sub);
+        // 4th index to last
         sub = str.substring(4);
         System.out.println(sub);
 

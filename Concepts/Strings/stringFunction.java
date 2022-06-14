e35b0f4858244c6f7fe2aaca25f22df49ba436be
Abhishek Mani <aabhishekmani@gmail.com>
2022-06-14 23:10:44 +0530

    changes

--- Concepts/Strings/stringFunction.java
+++ Concepts/Strings/stringFunction.java
@@ -15,12 +15,14 @@ class demoOnString{
 
     public void substring(String str){
         System.out.println(str);
-        // String contain 0th index to 8th which is "Abhishek".
+        // String contain 0th index to (8-1)th index which is "Abhishek".
         String sub = str.substring(0,8);
         System.out.println(sub);
         // 4th index to last
         sub = str.substring(4);
         System.out.println(sub);
+        // this will print empty string becasue 0 to 0 
+        System.out.println(str.substring(0,0));
 
     }
 }

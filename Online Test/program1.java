9c8f01ba77ca28c4c2bc8d8f84686f0877d34f40
Abhishek Mani <aabhishekmani@gmail.com>
2022-06-17 23:09:13 +0530

    Changes

--- Online Test/program1.java
+++ Online Test/program1.java
@@ -3,7 +3,8 @@
 public class program1 {
     public static void main(String[] args) {
         String str  = "fun&!! time"; 
-        operration(str);
+        String str1 = "I love dogs present at my home";
+        operration(str1);
     }
     public static void operration(String str){
         String s = str;
@@ -11,9 +12,10 @@ public class program1 {
         String word [] =  s.split(" ");
         for (int i = 0; i < word.length; i++) {
             String st = removePunctuations(word[i]);
-            if()
+            if(result.length()<st.length())
+            result = st;
         }
-
+        System.out.println(result);
     }
     public static String removePunctuations(String source) {
         return source.replaceAll("\\p{IsPunctuation}", "");

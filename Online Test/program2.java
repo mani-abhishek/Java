858afc55100eb8beada99fd9b2df6d907da225a8
Abhishek Mani <aabhishekmani@gmail.com>
2022-06-19 00:00:02 +0530

    Changes

--- Online Test/program2.java
+++ Online Test/program2.java
@@ -5,13 +5,20 @@ public class program2 {
     }
     public static void operation(String str){
         String s = str;
-        int count;
+        int count=0;
         String result = "";
-        for (int i = 0; i < s.length(); i++) {
-            count = 0;
+        for (int i = 0; i < s.length()-1; i++) {
+            
             if(s.charAt(i)==s.charAt(i+1)){
                 count++;
             }
+            else{
+                count = count+1;
+                String res = s.charAt(i)+Integer.toString(count);
+                result = result+res;
+                count = 0;
+            }
         }
+        System.out.println(result);
     }
 }

f1b0e4c9dae4fec79752e68971a33161eedf54f5
Abhishek Mani <aabhishekmani@gmail.com>
2022-06-18 23:49:15 +0530

    Changes

---
+++ Online Test/program2.java
@@ -0,0 +1,9 @@
+public class program2 {
+    public static void main(String[] args) {
+        String str = "aabbbccd";
+        operation(str);
+    }
+    public static void operation(String str){
+
+    }
+}

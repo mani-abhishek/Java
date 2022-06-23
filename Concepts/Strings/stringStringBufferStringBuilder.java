56617adfb731e2759e81d494c5651d15488db88a
Abhishek Mani <aabhishekmani@gmail.com>
2022-06-23 21:14:32 +0530

    Changes

---
+++ Concepts/Strings/stringStringBufferStringBuilder.java
@@ -0,0 +1,23 @@
+public class stringStringBufferStringBuilder {
+    public static void main(String[] args) {
+        String name = "Abhishek";
+        concat1(name);
+        StringBuilder name1 = new StringBuilder("Abhishek");
+        concat2(name1);
+        StringBuffer name2 = new StringBuffer("Abhishek");
+        concat3(name2);
+        System.out.println(name);
+        sys
+    }
+    public static void concat1(String s){
+        s = s+" Mani";
+    }
+    public static void concat2(StringBuilder s){
+        s.append(" Mani");
+    }
+    public static void concat3(StringBuffer s){
+        s.append(s);
+    }
+}
+
+

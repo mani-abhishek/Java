400ac24bd5858e55635a7ab30c897a46a25a0108
Abhishek Mani <aabhishekmani@gmail.com>
2022-06-24 23:35:44 +0530

    Changes

---
+++ Concepts/JAVA Bean/Bean.java
@@ -0,0 +1,28 @@
+/**
+ Beans are special type of Pojos. There are some restrictions on POJO to be a bean.
+
+1. All JavaBeans are POJOs but not all POJOs are JavaBeans.
+2. Serializable i.e. they should implement Serializable interface. Still, some POJOs who don’t implement a Serializable interface are called POJOs because Serializable is a marker interface and therefore not of many burdens.
+3. Fields should be private. This is to provide complete control on fields.
+4. Fields should have getters or setters or both.
+5. A no-arg constructor should be there in a bean.
+6. Fields are accessed only by constructor or getter setters.
+ */
+public class Bean {
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+/**
+ * InnerBean
+ */
+class javaBeanDemo{
+    private String name;
+    javaBeanDemo(){
+
+    }
+    
+    
+}

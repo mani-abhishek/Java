a37403c2488392bb063d44d1ced404d3adac45d1
Abhishek Mani <aabhishekmani@gmail.com>
2022-06-24 23:46:37 +0530

    Changes

---
+++ Concepts/Map/mapDemo.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+/**
+ * mapDemo
+ */
+import java.util.*;
+
+public class mapDemo {
+    public static void main(String[] args) {
+        HashMap <Integer,String>  map = new HashMap <>();
+        map.put(1,"Abhishek");
+        map.put(2,"Mani");
+        map.put(3,"Name");
+        map.put(4,"vicky");
+
+        System.out.println(map);
+    }
+
+}

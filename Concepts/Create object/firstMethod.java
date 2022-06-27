04f6ad640abd61f4b3a5f740d40c0d71b6cc02f7
Abhishek Mani <aabhishekmani@gmail.com>
2022-06-27 23:38:29 +0530

    Changes

---
+++ Concepts/Create object/firstMethod.java
@@ -0,0 +1,14 @@
+/**
+ * firstMethod
+ */
+public class firstMethod {
+
+    public static void main(String[] args) {
+        Student obj = new Student();
+        obj.name="Abhi";
+        System.out.println(obj.name);
+    }
+}
+class Student {
+    String name;
+}

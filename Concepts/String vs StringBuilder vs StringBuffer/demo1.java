1306427356e9aac317f9f13cd6ac6b52269757f2
Abhishek Mani <aabhishekmani@gmail.com>
2022-06-28 22:55:06 +0530

    Changes

--- Concepts/String vs StringBuilder vs StringBuffer/demo1.java
+++ Concepts/String vs StringBuilder vs StringBuffer/demo1.java
@@ -1,6 +1,7 @@
 public class demo1 {
     public static void main(String[] args) {
         StringBuffer s1 = new StringBuffer();
+        StringBuffer s3 = new StringBuffer("Abhi");
         s1.append("Abhi");
         System.out.println("String 1st = "+s1);
 
@@ -9,11 +10,11 @@ public class demo1 {
 
         System.out.println("String 2nd = "+s2);
 
-        System.out.println("Using .equals Method = "+s1.equals(s2));
-        // if(s1==s2)
+        System.out.println("Using .equals Method = "+s1.equals(s3));
+        if(s1==s3)
         System.out.println("Give an error");
-        System.out.println(s1);
-        System.out.println(s1.compareTo(s1.reverse()));
+        // System.out.println(s1.reverse());
+        System.out.println(s1.compareTo(s3));
         System.out.println(s1);
         
     }

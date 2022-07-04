09926724f222debf671724b80d4c3ca0ecdad58b
Abhishek Mani <aabhishekmani@gmail.com>
2022-07-04 23:35:49 +0530

    changes

--- Practice/Leetcode/removeParenthesis301.java
+++ Practice/Leetcode/removeParenthesis301.java
@@ -1,60 +1,56 @@
-
-/**
- * 301
- * 
- */
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Stack;
 
 public class removeParenthesis301 {
 
+    public String toString(){
+        return 
+    }
+
     public static void main(String[] args) {
-        System.out.println("welcome");
+        // System.out.println("welcome");
         String s = ")("; // Output: ["(())()","()()()"]
+        // String s = "()()))()"; 
+        // String s = "()()(()"; 
         Stack<Character> st = new Stack<>();
         List<String> list = new ArrayList<>();
         int i = 0;
-        String s1="",s2="";
-        int push =0;
+
         while (i < s.length()) {
             char ch = s.charAt(i);
-            if (ch == s.charAt(0)) {
+            if (ch == '(') {
                 st.push(ch);
-                s1 += ch;
-                push++;
             } else if (st.empty()) {
                 st.push(ch);
-                push++;
-                
-            } else {
+            } else if(st.peek()=='(') {
                 st.pop();
-                s1 += ch;
-                push=0;
-            }
-            if(push==2)
-            break;
+            }else st.push(ch);
             i++;
-            
         }
-        int p1 = 0;
-        while (s.charAt(i-1)==s.charAt(p1)) {
-            p1++;
-        }
-        System.out.println(st);
-        System.out.println(s1);
-        System.out.println(i+" "+s.charAt(i-1));
-        System.out.println(p1);
+        String ss = st.toString();
+        System.out.println(ss.charAt(0));
+        // System.out.println(st.toString());
+
+        // int p1 = 0;
+        // while (s.charAt(i-1)==s.charAt(p1)) {
+        //     p1++;
+        // }
+        // System.out.println(st);
+        // System.out.println(s1);
+        // System.out.println(i+" "+s.charAt(i-1));
+        // System.out.println(p1);
         
-        System.out.println(s.substring(0,i-1) + s.substring(i));
+        // System.out.println(s.substring(0,i-1) + s.substring(i));
 
 
-        System.out.println("-------------------------------");
+        // System.out.println("-------------------------------");
 
-        list.add(s.substring(0,p1) + s.substring(p1+1));
-        list.add(s.substring(0,i-1) + s.substring(i));
+        // list.add(s.substring(0,p1) + s.substring(p1+1));
+        // list.add(s.substring(0,i-1) + s.substring(i));
 
-        System.out.println(list);
+        // System.out.println(list);
 
     }
 }

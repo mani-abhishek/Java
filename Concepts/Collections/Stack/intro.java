f673315a55872b6687505cc63f595a8b9ce59d44
Abhishek Mani <aabhishekmani@gmail.com>
2022-07-05 23:17:41 +0530

    Changes

--- Concepts/Collections/Stack/intro.java
+++ Concepts/Collections/Stack/intro.java
@@ -1,8 +1,9 @@
+import java.util.Stack;
 
-
-package Stack;
 
 public class intro {
+    public static void main(String[] args) {
+        System.out.println("------------------------------= Welcome to Stack =------------------------------");
+    }
 
-    
 }

a867010137bc89bbfe61bad7406e10603964d94f
Abhishek Mani <aabhishekmani@gmail.com>
2022-07-05 23:22:56 +0530

    Changes

--- Concepts/Collections/Stack/intro.java
+++ Concepts/Collections/Stack/intro.java
@@ -4,6 +4,15 @@ import java.util.Stack;
 public class intro {
     public static void main(String[] args) {
         System.out.println("------------------------------= Welcome to Stack =------------------------------");
+        Stack <Integer> st = new Stack<>();  // Declaration of Stack 
+        
+
+
+        //List of method
+        st.push(3);  //add data into stack
+        st.push(5); //add data into stack
+        System.out.println(st.pop()); //remove data from stack
+        System.out.println(st.peek()); // to see top of the stack
     }
 
 }

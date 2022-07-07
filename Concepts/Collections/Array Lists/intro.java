6b5471b98e506e5e5f7560ad3c7c06d9cb90be4a
Abhishek Mani <aabhishekmani@gmail.com>
2022-07-07 23:36:15 +0530

    changes

--- Concepts/Collections/Array Lists/intro.java
+++ Concepts/Collections/Array Lists/intro.java
@@ -8,5 +8,15 @@ public class intro {
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
+        list.add(9);
+        list.add(7);
+        list.add(2);
+        list.add(5);
+        list.add(3);
+
+
+        list.add(4, 8);
+
+        System.out.println(list);
     }
 }

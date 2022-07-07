8f05f438a64facad7fe5e40459de70ac67f3d436
Abhishek Mani <aabhishekmani@gmail.com>
2022-07-07 23:27:12 +0530

    changes

--- Concepts/Collections/Array Lists/intro.java
+++ Concepts/Collections/Array Lists/intro.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * intro
  */
 public class intro {
 
     public static void main(String[] args) {
-        
+        ArrayList<Integer> list = new ArrayList<>();
     }
 }

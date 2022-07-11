2277057315d00795e82123d0b50006a54bf8fc30
Abhishek Mani <aabhishekmani@gmail.com>
2022-07-11 20:41:44 +0530

    Changes

--- Concepts/Collections/Array Lists/intro.java
+++ Concepts/Collections/Array Lists/intro.java
@@ -19,17 +19,5 @@ public class intro {
         // System.out.println(list);
         fun();
     }
-    public static void fun(){
-        String face= "N";
-        String dir = "NESW";
-        int ind = dir.indexOf(face);
-        System.out.println(ind);
-
-        String [] str = {"A","B","C"};
-        String st = "ABC";
-        int i = st.indexOf("B");
-        System.out.println(st.indexOf("B"));
-        System.out.println(st.substring(0,1));
-        System.out.println(st.substring(i-1,i));
-    }
+   
 }

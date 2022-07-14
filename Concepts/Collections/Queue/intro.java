0ce759c15ed0615a4d17cf792394e912b6df9c73
Abhishek Mani <aabhishekmani@gmail.com>
2022-07-14 23:56:29 +0530

    Changes

--- Concepts/Collections/Queue/intro.java
+++ Concepts/Collections/Queue/intro.java
@@ -5,6 +5,7 @@ public class intro {
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>();
         q.add(10);
+        q.poll();
         System.out.println(q);
     }
 }

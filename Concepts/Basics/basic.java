f6f3a089f0899b0e27f25cab6c445896f0d32cc6
Abhishek Mani <aabhishekmani@gmail.com>
2022-07-19 20:10:57 +0530

    Changes

---
+++ Concepts/Basics/basic.java
@@ -0,0 +1,10 @@
+public class basic {
+    public static void main(String[] args) {
+        System.out.print("abhi");
+        // System.out.println();
+        // System.out.print("\b\b"); // Prints a backspace
+        for(int i = 0; i < 80*300; i++) // Default Height of cmd is 300 and Default width is 80
+           System.out.print("\b"); // Prints a backspace
+           System.out.print("dddfgdfgs");
+    }
+}

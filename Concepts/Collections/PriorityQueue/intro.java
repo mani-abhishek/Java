c771cf470a32b25a52203d69b747557a6623c35b
Abhishek Mani <aabhishekmani@gmail.com>
2022-07-20 23:20:44 +0530

    Changes

---
+++ Concepts/Collections/PriorityQueue/intro.java
@@ -0,0 +1,10 @@
+import java.util.PriorityQueue;
+
+public class intro {
+    public static void main(String[] args) {
+        
+    }
+    public static void demo(){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+    }
+}

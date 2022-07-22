b19c122f5cd57e27aaeba401090f5ac83b4f6315
Abhishek Mani <aabhishekmani@gmail.com>
2022-07-22 23:47:45 +0530

    CHanges

--- Concepts/Collections/PriorityQueue/intro.java
+++ Concepts/Collections/PriorityQueue/intro.java
@@ -1,3 +1,6 @@
+// by default it implements Max Heap
+
+
 import java.util.PriorityQueue;
 
 public class intro {
@@ -6,5 +9,12 @@ public class intro {
     }
     public static void demo(){
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(10);
+        pq.add(20);
+        pq.add(30);
+        pq.add(50);
+        pq.add(1);
+        System.out.println(pq.peek());
+        
     }
 }

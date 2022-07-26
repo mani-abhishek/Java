32c6366d697b1b0c3dbdc6daeb1a6a9692653267
Abhishek Mani <aabhishekmani@gmail.com>
2022-07-26 23:32:21 +0530

    Changes

--- Concepts/Array/intro.java
+++ Concepts/Array/intro.java
@@ -3,12 +3,21 @@
  */
 public class intro {
     public static void main(String[] args) {
+        // 1-D Array
         System.out.println("welecome to Array"); 
         int a[] = new int[10];
         int b[];
         b = new int [1];
         int c[]={1,34,5,6};
-        
 
+        // 2-D Array
+        int z[][] = new int[1][3];
+
+
+    }
+    public static void print2D(int a[][]){
+        for (int i = 0; i < a.length; i++) {
+            
+        }
     }
 }

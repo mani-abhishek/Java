6e0549ebf8239337106b6409a7aff32fb97b4769
Abhishek Mani <aabhishekmani@gmail.com>
2022-07-26 23:28:34 +0530

    Changes

--- Concepts/Array/intro.java
+++ Concepts/Array/intro.java
@@ -3,6 +3,12 @@
  */
 public class intro {
     public static void main(String[] args) {
-        System.out.println("elecome"); 
+        System.out.println("welecome to Array"); 
+        int a[] = new int[10];
+        int b[];
+        b = new int [1];
+        int c[]={1,34,5,6};
+        
+
     }
 }

6d18a4c242a052cc971879e07c65453d4870ec80
Abhishek Mani <aabhishekmani@gmail.com>
2022-08-06 20:17:47 +0530

    Changes

---
+++ Company/Tiger Analytics/bitwise.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class bitwise {
+    public static void main(String[] args) {
+        int input[]= {1,2,3};
+        ArrayList <Integer> list = new ArrayList<>();
+        int n = input.length;
+        for (int i = 0; i < input.length; i++) {
+            
+        }
+
+       
+    }
+    
+}
---
+++ Company/Tiger Analytics/lex.java
@@ -0,0 +1,52 @@
+// A simple Java program to find
+// lexicographically minimum rotation
+// of a given String
+import java.util.*;
+
+public class lex
+{
+
+	static String minLexRotation(String str)
+	{
+         System.out.println("Input = "+str);
+		int n = str.length();
+
+		String arr[] = new String[n];
+
+		String concat = str + str;
+        System.out.println("After concatination = "+concat);
+
+		for (int i = 0; i < n; i++)
+		{
+            arr[i] = concat.substring(i, i + n);
+            System.out.println(arr[i]);
+		}
+
+		Arrays.sort(arr);
+        System.out.println("After sorting :  "+Arrays.toString(arr));
+		return arr[0];
+	}
+
+	// Driver code
+	public static void main(String[] args)
+	{
+		// System.out.println(minLexRotation("GEEKSFORGEEKS"));
+		// System.out.println(minLexRotation("GEEKSQUIZ"));
+      
+		// System.out.println("Final Output = "+minLexRotation("babcbb"));
+        myFun("GEEKSFORGEEKS");
+	}
+
+    static void myFun(String str ){
+        int len = str.length();
+        String temp = str +str;
+        String arr[] = new String[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = temp.substring(i,i+len);
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0]);
+    }
+}
+
+

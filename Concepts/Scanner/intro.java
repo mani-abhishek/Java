0f151e0cc3c4a7fe10ad3fcd33c793c07d0e1f5a
Abhishek Mani <aabhishekmani@gmail.com>
2022-08-09 23:31:21 +0530

    Changes

--- Concepts/Scanner/intro.java
+++ Concepts/Scanner/intro.java
@@ -1,25 +1,35 @@
 import java.util.Scanner;
 
-
 public class intro {
-   
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         try {
             // System.out.println("Enter Name =");
-        int age = sc.nextInt();
-        System.out.println("Your age is "+age);
+            int age = sc.nextInt();
+            System.out.println("Your age is " + age);
+            // method1(sc);
+            specialCase(sc);
         } catch (Exception e) {
-            //TODO: handle exception
+            // TODO: handle exception
         }
 
-
-        finally{
+        finally {
             sc.close();
         }
     }
-    public static void method1(){
+
+    private static void specialCase(Scanner sc) {
+        int n = sc.nextInt();
+        String name = sc.nextLine();
+        sc.nextLine();
+        for (int i = 0; i < 5; i++) {
+            String a = sc.nextLine();
+            System.out.println(i+" = "+a);
+        }
+    }
+
+    public static void method1(Scanner sc) {
         // take char input in java using java
         char c = sc.next().charAt(0);
     }

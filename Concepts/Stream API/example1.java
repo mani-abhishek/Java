d1b9a4d6aeddbe8e5d49b56a4093126e744d72c3
Abhishek Mani <aabhishekmani@gmail.com>
2022-08-11 23:33:12 +0530

    Changes

--- Concepts/Stream API/example1.java
+++ Concepts/Stream API/example1.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class example1 {
     public static void main(String[] args) {
@@ -12,12 +13,20 @@ public class example1 {
         emp.add(new employee("Ankit", 21, "male", "Civil"));
         emp.add(new employee("Vandana", 18, "female", "mechanical"));
         emp.add(new employee("Siddharth", 26, "male", "IT"));
+        emp.add(new employee("Siddharth", 26, "male", "IT"));
 
 
-
-        emp.stream().map(employee::getDepartment).distinct().forEach(System.out::println);;
-        // System.out.println(emp);
+        // // find distinct department name
+        // emp.stream().map(employee::getDepartment).distinct().forEach(System.out::println);
         
+        // //Count how many employee each department have.
+        // Map <String,Long> departmentCount = 
+        // emp.stream().collect(Collectors.groupingBy(employee::getDepartment,Collectors.counting()));
+        // System.out.println(departmentCount);
+        
+        //find average age of male and female
+        Map <String,Double> grnderAverage = emp.stream().collect(Collectors.groupingBy(employee::getGender,Collectors.averagingDouble(employee::getAge)));
+        System.out.println(grnderAverage);
         
     }
 }

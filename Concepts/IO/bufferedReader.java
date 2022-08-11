2c913d22cc21c55b4e50edc41c9f9e2dd2e44e34
Abhishek Mani <aabhishekmani@gmail.com>
2022-08-11 22:34:13 +0530

    Changes

--- Concepts/IO/bufferedReader.java
+++ Concepts/IO/bufferedReader.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 
 public class bufferedReader {
     public static void main(String[] args) throws IOException {
-        InputStreamReader in = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(in);
+        // InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter");
         int size = Integer.parseInt(br.readLine());
         String word = br.readLine();
         String sen[] = new String[size];
-        String line;
         for (int i = 0; i < size; i++) {
             sen[i] = br.readLine();
         }

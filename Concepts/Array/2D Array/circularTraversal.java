c4dda871adb7cb0834e9c22df058e57c75a6d509
Abhishek Mani <aabhishekmani@gmail.com>
2022-08-19 23:23:52 +0530

    Changes

--- Concepts/Array/2D Array/circularTraversal.java
+++ Concepts/Array/2D Array/circularTraversal.java
@@ -2,50 +2,47 @@ import java.util.Arrays;
 
 public class circularTraversal {
     public static void main(String[] args) {
-        int arr[][]={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}};
+        int arr[][]={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}};
         // output 1,2,3,4,5,10,15,14,13,12,11,6,7,8,9
-        int n = 4;
-        int m = 5;
+        int r = 4;
+        int c = 5;
         printList(arr);
-        solve(arr,n,m);
+        solve(arr,r,c);
     }
-    private static void solve(int[][] arr, int m, int n) {
-        int x=0;
-        int len = m*n;
-        int i=0;
-        int j=0;
-        int a=m;
-        int b=n;
-        while(x<=len){
-            while(j<b&&x<=len){
+    private static void solve(int[][] arr, int r, int c) {
+        int i=0,j=0,x=1,y=0,k=0;
+        int l = r*c;
+        while(k<l){
+            while(k<l&&j<c){
                 System.out.print(arr[i][j++]+",");
-                x++;
+                k++;
             }
-            i++;
-            j--;
-            while(x<=len&&i<a){
+            i = i+1;
+            j = j-1; 
+            c=c-1;
+            while(k<l&&i<r){
                 System.out.print(arr[i++][j]+",");
-                x++;
+                k++;
             }
-            i--;
-            while(x<=len&&j>=0){
-                System.out.print(arr[i][j--]+",");
-                x++;
-            }
-            j=j+1;
+            // System.out.println(i+"-"+j);
             i=i-1;
-            while(x<=len&&i>=1){
-                System.out.print(arr[i--][j]+",");
-                x++;
+            j= j-1;
+            r=r-1;
+            while(k<l&&j>=y){
+                System.out.print(arr[i][j--]+",");
+                k++;
             }
+            y=y+1;
+            i=i-1;
+            j=j+1;
+            while(k<l&&i>=x){
+                System.out.print(arr[i--][j]+",");
+                k++;
+            }
+            x=x+1;
             i=i+1;
             j=j+1;
-            a=a+1;
-
-            // System.out.println(i+" = "+j);
-            // break;
         }
-        
     }
     private static void printList(int arr[][]){
         for(int a[]:arr)
